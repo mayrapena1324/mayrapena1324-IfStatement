c65b69b18655af9f6e7d331eaa11df9b13bf844c
Mayra Pena(Peña) <mayrapena1324@gmail.com>
2023-01-26 04:43:33 +0000

    Add if else statement

--- src/main/java/IfStatement.java
+++ src/main/java/IfStatement.java
@@ -19,7 +19,12 @@ public class IfStatement {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
+    
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        if (bool) {
+            return x;
+        } else {
+            return y;
+        }
     }
 }
